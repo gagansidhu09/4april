40038404c55e82e1a613e083e8c36c6405841030
Gaganpreet Singh Sidhu <703141@student.tor.loc>
2019-04-04 10:49:01 -0400

    node file

---
+++ nodefile.js
@@ -0,0 +1,10 @@
+var user = {
+    first_name: "Gaganpreet",
+    last_name: "Sidhu",
+    age:4,/*from w w w. java 2 s.c om*/
+    website: "java2s.com"
+};
+
+for (key in user) {
+    console.log(key);
+}
